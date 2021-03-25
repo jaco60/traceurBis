8154b1892b01397516c950ab0d37dec1574a9fa7
jaco60 <jacoboni@univ-tlse2.fr>
2021-03-25 21:50:45 +0100

    Work on North

--- tests/traceur_test.go
+++ tests/traceur_test.go
@@ -9,14 +9,14 @@ import (
 )
 
 func TestNorth(t *testing.T) {
+	h // https://goplay.space/#rqe8MaGVWd3
 	Init()
-	Right()
+	Down("red")
 	Forward(5)
-	North()
-	if AngleToReturnToNorth != 0 {
-		t.Errorf("Forward failed - expected 0 - got x=%d", AngleToReturnToNorth)
-	}
+	Say("Je tourne à 3O° et j'avance de 2 pas")
 	Pivote(30)
+	Forward(2)
+	Say("Je repars vers le nord et j'avance de 3 pas")
 	North()
 	if AngleToReturnToNorth != 0 {
 		t.Errorf("Forward failed - expected 0 - got x=%d", AngleToReturnToNorth)
